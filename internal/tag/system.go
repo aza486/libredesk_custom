34f99ffd33239731daa37067820334c1fa26fc59
aza486 <d.podjapolski@web.de>
2026-07-24 10:28:14 +0000

    system tags nad css

--- internal/tag/system.go
+++ internal/tag/system.go
@@ -3,16 +3,19 @@ package tag
 const (
 	SystemTagInternal = 8
 	SystemTagCustomer = 9
+	SystemTagHuman    = 11
 )
 
 var systemTagIDs = map[int]struct{}{
 	SystemTagInternal: {},
 	SystemTagCustomer: {},
+	SystemTagHuman:    {},
 }
 
 var systemTagNames = map[string]struct{}{
-	"🏢Intern":        {},
-	"🦽Kundenticket": {},
+    "🏢Intern": {},
+    "🦽Kundenticket": {},
+    "🧙🏼Mensch erforderlich": {},
 }
 
 func IsSystemTag(id int) bool {
